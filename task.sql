9f4d8af65940ba9fcf4f0bfcce5dcdfe52af9855
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-01-28 20:03:09 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,6 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT INSERT, UPDATE, DELETE, SELECT ON database.table TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL ON database.table TO 'deploymentuser'@'%';
+
